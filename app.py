bd1935bd48520736e995d4354117b19fcaa3981f
Aileron64 <alextcp@hotmail.com>
2023-01-11 20:02:26 -0500

    added subprocess

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import sys
 import os 
 import json
+import subprocess
 
+#from Seedcounter.seedcounter import SeedCounter
 from PySide2.QtCore import QObject, Slot, Signal
-from seedcounter import SeedCounter
 
 class App(QObject):
     def __init__(self, parent=None):
         super(App, self).__init__(parent)
-
-    # API Functions
-    #signalExample = Signal(str)
 
     @Slot(str)
     def setMode(self, example):
@@ -18,10 +16,8 @@ class App(QObject):
         #self.signalExample.emit(example)
 
     @Slot(str, str)
-    def runSeedCounter(self, input, output)
-        counter = SeedCounter()
-        counter.process_folder(folder_in = input, folder_out = output)
+    def runSeedCounter(self, input, output):
+        print(input)
+        print(output)
 
-# if __name__ == "__main__":
-#     counter = SeedCounter()
-#     counter.process_folder(folder_in = "testImages",folder_out = "seedcounter_imgs")
+        p = subprocess.run(["python3", "seedcounter.py"])
